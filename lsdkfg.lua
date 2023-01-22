8bab7a28111377d6145d4bf06f1d27cde23bd1ce
zezinxpinto <121965166+zezinxpinto@users.noreply.github.com>
2023-01-21 21:55:59 -0300

    Create lsdkfg.lua

---
+++ lsdkfg.lua
@@ -0,0 +1,19 @@
+for i, v in pairs(game:GetService('Teams'):GetDescendants()) do
+			if v:IsA('Tool') then
+				v.Parent = game:GetService('Players').LocalPlayer.Backpack
+			end
+		end
+		game:GetService('Players').LocalPlayer.Character.Humanoid.Died:Connect(function()
+			for i, v in pairs(game:GetService('Players').LocalPlayer.Backpack:GetDescendants()) do
+				if v:IsA('Tool') then
+					v.Parent = game:GetService('Teams')
+				end
+			end
+			for i, v in pairs(game:GetService('Players').LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('Tool') then
+					v.Parent = game:GetService('Teams')
+				end
+			end
+		end)
+		
+	end)
